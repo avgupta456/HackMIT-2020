c885a738cff0794272b9b14afa4d3751650a115c
Abhijit Gupta <avgupta456@gmail.com>
2020-09-20 04:06:51 -0400

    backend hooked up correctly

--- frontend/src/Person.js
+++ frontend/src/Person.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import CameraIcon from "@material-ui/icons/PhotoCamera";
@@ -15,13 +15,23 @@ import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import { Link } from "react-router-dom";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
+
+import axios from "axios";
+import FormData from "form-data";
+import MicRecorder from "mic-recorder-to-mp3";
+
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+// const backend = "https://hackmit-2020-290013.ue.r.appspot.com";
+const backend = "http://localhost:5000";
+const name = "William-Shakespeare";
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant='body2' color='textSecondary' align='center'>
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color='inherit' href='https://material-ui.com/'>
         Your Website
       </Link>{" "}
       {new Date().getFullYear()}
@@ -65,7 +75,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
     borderRadius: "12px",
     boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
@@ -85,54 +95,151 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Person() {
   const classes = useStyles();
+  const [text, setText] = useState("");
+  const [blobUrl, setBlobUrl] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const [isBlocked, setIsBlocked] = useState(false);
+
+  const start = () => {
+    if (isBlocked) {
+      console.log("Permission Denied");
+    } else {
+      Mp3Recorder.start()
+        .then(() => {
+          setIsRecording(true);
+        })
+        .catch((e) => console.error(e));
+    }
+  };
+
+  const stop = async () => {
+    const [buffer, blob] = await Mp3Recorder.stop().getMp3();
+    console.log(blob);
+
+    const formData = new FormData();
+    formData.append("audio", blob);
+    let url = backend + "/get_response?name=" + name;
+    console.log(url);
+    const text = await axios.post(url, formData);
+    console.log(text);
+    url = backend + "/get_audio?text=" + text["data"].replace(" ", "-");
+    const response = await axios.post(url, formData, { responseType: "blob" });
+    console.log(response);
+    const blobURL = URL.createObjectURL(response.data);
+    console.log(blobURL);
+    setText(text);
+    setBlobUrl(blobURL);
+    setIsRecording(false);
+  };
+
+  const toggle = () => {
+    if (isRecording) {
+      stop();
+    } else {
+      start();
+    }
+  };
+
+  useEffect(() => {
+    navigator.getUserMedia(
+      { audio: true },
+      () => {
+        console.log("Permission Granted");
+        setIsBlocked(false);
+      },
+      () => {
+        console.log("Permission Denied");
+        setIsBlocked(true);
+      }
+    );
+  });
 
   return (
     <React.Fragment>
       <CssBaseline />
 
       <main>
-      <Grid style={{padding: "100px"}} container spacing={3}>
-        
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-            
-          <img alt="Elon Musk" src="/images/elon_musk.jpg" style={{borderRadius: "5%"}}/>
-            <h2>Elon Musk</h2>
-            <p>Engineer, Entrepreneur, Inventor.</p>
-            <div style={{textAlign: 'left'}} >
-            <p>Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pretium hendrerit mattis. Cras vulputate libero in dapibus porta. Cras eget dignissim tellus. Duis augue justo, condimentum id scelerisque ultrices, auctor vel diam. </p>
-            </div>
-          </Paper>
-          
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
+        <Grid style={{ padding: "100px" }} container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <img
+                alt='Elon Musk'
+                src='/images/elon_musk.jpg'
+                style={{ borderRadius: "5%" }}
+              />
+              <h2>Elon Musk</h2>
+              <p>Engineer, Entrepreneur, Inventor.</p>
+              <div style={{ textAlign: "left" }}>
+                <p>
+                  Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Cras pretium hendrerit mattis. Cras vulputate libero in
+                  dapibus porta. Cras eget dignissim tellus. Duis augue justo,
+                  condimentum id scelerisque ultrices, auctor vel diam.{" "}
+                </p>
+              </div>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <h2>Transcript</h2>
+              <button onClick={() => toggle()}>
+                <img
+                  alt='Mic'
+                  src='/images/mic.png'
+                  width='17%'
+                  height='auto'
+                />
+              </button>
+              <audio src={blobUrl} controls='controls' />
+              <div style={{ textAlign: "left" }}>
+                <p>
+                  Elon Musk: Lorem ipsum dolor sit amet, consectetur adipiscing
+                  elit. Cras pretium hendrerit mattis. Cras vulputate libero in
+                  dapibus porta. Cras eget dignissim tellus. Duis augue justo,
+                  condimentum id scelerisque ultrices, auctor vel diam. Donec
+                  sed elit sed lectus tempor ultrices. Aliquam a enim id erat
+                  varius vestibulum non viverra nibh.{" "}
+                </p>
+                <p>
+                  Me: Integer placerat libero malesuada, consequat neque id,
+                  eleifend leo. Proin eu egestas lacus. Fusce ullamcorper dui
+                  eget ultrices bibendum. Aenean eu turpis commodo, dignissim
+                  dolor eu, eleifend mauris. Cras dictum erat vel posuere
+                  finibus. Aenean quis congue augue. Sed pharetra sodales
+                  pharetra.{" "}
+                </p>
 
-          <h2>Transcript</h2>
-            <img alt="Mic" src="/images/mic.png" width="17%" height="auto"/>
-            <div style={{textAlign: 'left'}} >
-            <p>Elon Musk: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pretium hendrerit mattis. Cras vulputate libero in dapibus porta. Cras eget dignissim tellus. Duis augue justo, condimentum id scelerisque ultrices, auctor vel diam. Donec sed elit sed lectus tempor ultrices. Aliquam a enim id erat varius vestibulum non viverra nibh. </p>
-            <p>Me: Integer placerat libero malesuada, consequat neque id, eleifend leo. Proin eu egestas lacus. Fusce ullamcorper dui eget ultrices bibendum. Aenean eu turpis commodo, dignissim dolor eu, eleifend mauris. Cras dictum erat vel posuere finibus. Aenean quis congue augue. Sed pharetra sodales pharetra. </p>
-            
-            <p>Elon Musk: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pretium hendrerit mattis. Cras vulputate libero in dapibus porta. Cras eget dignissim tellus. Duis augue justo, condimentum id scelerisque ultrices, auctor vel diam. Donec sed elit sed lectus tempor ultrices. Aliquam a enim id erat varius vestibulum non viverra nibh. </p>
-            <p>Me: Integer placerat libero malesuada, consequat neque id, eleifend leo. Proin eu egestas lacus. Fusce ullamcorper dui eget ultrices bibendum. Aenean eu turpis commodo, dignissim dolor eu, eleifend mauris. Cras dictum erat vel posuere finibus. Aenean quis congue augue. Sed pharetra sodales pharetra. </p>
-            </div>
-          </Paper>
+                <p>
+                  Elon Musk: Lorem ipsum dolor sit amet, consectetur adipiscing
+                  elit. Cras pretium hendrerit mattis. Cras vulputate libero in
+                  dapibus porta. Cras eget dignissim tellus. Duis augue justo,
+                  condimentum id scelerisque ultrices, auctor vel diam. Donec
+                  sed elit sed lectus tempor ultrices. Aliquam a enim id erat
+                  varius vestibulum non viverra nibh.{" "}
+                </p>
+                <p>
+                  Me: Integer placerat libero malesuada, consequat neque id,
+                  eleifend leo. Proin eu egestas lacus. Fusce ullamcorper dui
+                  eget ultrices bibendum. Aenean eu turpis commodo, dignissim
+                  dolor eu, eleifend mauris. Cras dictum erat vel posuere
+                  finibus. Aenean quis congue augue. Sed pharetra sodales
+                  pharetra.{" "}
+                </p>
+              </div>
+            </Paper>
+          </Grid>
         </Grid>
-        
-      </Grid>
-
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
+        <Typography variant='h6' align='center' gutterBottom>
           Footer
         </Typography>
         <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
+          variant='subtitle1'
+          align='center'
+          color='textSecondary'
+          component='p'
         >
           Something here to give the footer a purpose!
         </Typography>
